b620e7df02554f43f53a082fec536c15532e4033
aidaa2007 <aidaakrmbb@outlook.com>
2026-06-13 03:01:44 +0600

    .

---
+++ practice2/boolean/boolean_comparison.py
@@ -0,0 +1,16 @@
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+  
+  
+x = 10
+y = 12
+
+print("x == y:", x == y)
+print("x != y:", x != y)
+print("x > y:", x > y)
+print("x <= y:", x <= y)
