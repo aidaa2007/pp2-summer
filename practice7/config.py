72ea3d42b9306d812305361f2bbe662f2809243d
aidaa2007 <aidaakrmbb@outlook.com>
2026-06-30 16:55:45 +0600

    .

---
+++ practice7/config.py
@@ -0,0 +1,8 @@
+def load_config():
+    return {
+        "host": "localhost",
+        "database": "phonebook",
+        "user": "postgres",
+        "password": "your_password",
+        "port": 5432
+    }
