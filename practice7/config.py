9510ce63bf95a93984e8cbde7315e4029a167088
aidaa2007 <aidaakrmbb@outlook.com>
2026-07-02 12:00:10 +0600

    .

--- practice7/config.py
+++ practice7/config.py
@@ -1,8 +1,18 @@
-def load_config():
-    return {
-        "host": "localhost",
-        "database": "phonebook",
-        "user": "postgres",
-        "password": "your_password",
-        "port": 5432
-    }
+from configparser import ConfigParser
+
+
+def load_config(filename="database.ini", section="postgresql"):
+    parser = ConfigParser()
+    parser.read(filename)
+
+    db = {}
+
+    if parser.has_section(section):
+        params = parser.items(section)
+
+        for param in params:
+            db[param[0]] = param[1]
+    else:
+        raise Exception(f"Section {section} not found.")
+
+    return db
